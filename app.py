b3b78dcfb43ef6e80978512451257be3e50b4e89
Paras <guptaparas039@gmail.com>
2020-09-16 23:01:55 +0530

    added database

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, template_folder='templates')
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.sqlite3"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
